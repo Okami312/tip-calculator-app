e5005624ac5d3c38f8cd34be565c6d92bddb1076
Okami312 <baltaglaurentiu99@gmail.com>
2025-03-26 20:51:17 +0200

    26.03

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 const form = document.querySelector(".calculator-container form");
 const amountInputError = document.querySelector(".amount-input-error");
 const tipSelector = document.querySelector(".select-tip");
+const totalPerPersonElement = document.querySelector(".total-per-person");
 
-let tipPercentage = 0;
-
-const handleTipSelection = (e) => {
-  if (e.target.tagName.toLowerCase() === "button") {
-    const tipText = e.target.textContent.replace("%", "");
-    const tipValue = parseFloat(tipText);
-    if (!isNaN(tipValue)) {
-      tipPercentage = tipValue / 100;
-      console.log(`Tip percentage set to ${tipPercentage}`);
-    }
-  }
-};
+let selectedTip;
 
 const handleFormSubmit = (e) => {
   e.preventDefault();
@@ -30,13 +20,74 @@ const handleFormSubmit = (e) => {
     amountInputError.style.display = "none";
   }
 
+  // const numberOfPeople = Number(data.get("number-of-people"));
+  const totalPerPerson =
+    (amount + (selectedTip / 100) * amount) / numberOfPeople;
+  totalPerPersonElement.innerText = "$" + totalPerPerson.toFixed(2);
+
   const tipAmount = amount * tipPercentage;
   const tipPerPerson =
     numberOfPeople > 0 ? tipAmount / numberOfPeople : tipAmount;
 
   console.log(`Total Tip: ${tipAmount}, Tip per person: ${tipPerPerson}`);
+  resetButton.removeAttribute("disabled");
   //   console.log(typeof data.get("amount"));
+};
+
+let tipPercentage = 0;
+
+const handleTipSelection = (e) => {
+  if (e.target.tagName.toLowerCase() === "button") {
+    const tipText = e.target.textContent.replace("%", "");
+    const tipValue = parseFloat(tipText);
+    if (!isNaN(tipValue)) {
+      tipPercentage = tipValue / 100;
+      console.log(`Tip percentage set to ${tipPercentage}`);
+    }
+  }
 };
 
 form.addEventListener("submit", handleFormSubmit);
 tipSelector.addEventListener("click", handleTipSelection);
+
+// tip buttons
+
+const tipButtons = document.querySelectorAll(".tip-button");
+
+const handleSelectTip = (e) => {
+  tipButtons.forEach((tipButton) => {
+    tipButton.classlist.remove("percentage-button");
+  });
+  e.target.classlist.add("percentage-button");
+  selectedTip = Number(e.target.getAttribute("value"));
+};
+
+tipButtons.forEach((tipButton) => {
+  tipButton.addEventListener("click", handleSelectTip);
+});
+
+// for (let i = 0; i < tipButtons.length; i++) {
+//   tipButtons[i].addEventListener("click", handleSelectTip);
+// }
+
+// custom tip input
+const customTipInput = document.querySelector(".custom-tip-input");
+
+const handleCustomTip = () => {
+  tipButtons.forEach((tipButton) => {
+    tipButton.classlist.remove("percentage-button");
+  });
+
+  // selectedTip = Number(customTipInput.value);
+  // const selectedTipButton = document.querySelector(".percentage-button");
+  // if (selectedTipButton !== null) {
+  //   selectedTipButton.classlist.remove("percentage-button");
+  // }
+};
+
+customTipInput.addEventListener("input", handleCustomTip);
+
+const resetButton = document.querySelector(".reset-button");
+const handleResetForm = () => {};
+
+resetButton.addEventListener("click", handleResetForm);
